2500b47c3f55aa40f940dd662451829e865bbe73
Yousef Mardrosyan <104654454+azvali@users.noreply.github.com>
2024-10-22 21:01:08 -0400

    ddd

--- dashboard_app/app.py
+++ dashboard_app/app.py
@@ -1,38 +1,58 @@
-from flask import Flask, render_template, request, jsonify
-import base64
-
+from flask import Flask, render_template, request, jsonify, g
 app = Flask(__name__)
+import base64
+import asyncio
 
-# Global variables to hold the base64 image and description
-base64_image = ""
+
+
+def get_default_base64_image():
+    with open("uploads/parking-lot-facebook.jpg", "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+    
+
+
+base64_image = get_default_base64_image()
 description = "Fake parking lot"
-
+    
 @app.route('/')
 def home():
-    global base64_image
-    global description
-    return render_template('index.html', base64_image=base64_image, description=description)
+    global base64_image 
+    global description 
+    print("home endpoint: " + base64_image[:10])
+    
+    return render_template('index.html', base64_image = base64_image, description = description)
 
-# Route to receive base64 string and description from Raspberry Pi
+
+
 @app.route('/submit', methods=['POST'])
-def submit():
+async def submit():
+ 
     global base64_image
     global description
-
+    print("dog")
+    # Ensure the request contains JSON
     if not request.is_json:
         return jsonify({'status': 'error', 'message': 'Request must be JSON'}), 400
-
-    data = request.get_json()
     
-    # Update the image and description with incoming data from the Raspberry Pi
-    base64_image = data.get('image', base64_image)  # Update base64 string
-    description = data.get('description', description)  # Update description
+    # Get the data from the request
+    data = request.get_json()
 
-    return jsonify({
+    description = data.get('description', None)
+    base64_image = data.get('image', None)
+    
+    print("submit endpoint: " + base64_image[:10])
+    if not description or not base64_image:
+        return jsonify({'status': 'error', 'message': 'Missing description or image data'}), 400
+
+    # Send a response indicating success
+    response = {
         'status': 'success',
         'description': description,
-        'image': base64_image
-    })
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
+
