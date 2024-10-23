cf512763d02015f29c20e49c74f04148655d357a
StefaniakChristopher <stefaniakchristopher@gmail.com>
2024-10-22 20:04:51 -0400

    g implementaiton

--- dashboard_app/app.py
+++ dashboard_app/app.py
@@ -1,32 +1,29 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, g
 app = Flask(__name__)
 import base64
+import asyncio
+
+
 
 def get_default_base64_image():
     with open("uploads/parking-lot-facebook.jpg", "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
     
 
-base64_image = get_default_base64_image()
-description = "This is a parking lot"
+g.base64_image = get_default_base64_image()
+g.description = "No Data Found"
     
 @app.route('/')
 def home():
-
-    global base64_image
-    global description
-
     print("home endpoint: " + base64_image[:10])
     
-    return render_template('index.html', base64_image = base64_image, description = description)
+    return render_template('index.html', base64_image =g.base64_image, description =g.description)
 
 
 
 @app.route('/submit', methods=['POST'])
 async def submit():
-
-    global description
-    global base64_image
+ 
     print("dog")
     # Ensure the request contains JSON
     if not request.is_json:
@@ -37,10 +34,14 @@ async def submit():
 
     description = data.get('description', None)
     base64_image = data.get('image', None)
+    
     print("submit endpoint: " + base64_image[:10])
     if not description or not base64_image:
         return jsonify({'status': 'error', 'message': 'Missing description or image data'}), 400
     
+
+    g.base64_image = base64_image
+    g.description = description
     # Send a response indicating success
     response = {
         'status': 'success',
