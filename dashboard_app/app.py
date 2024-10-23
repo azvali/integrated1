3abd9d7abdd6d24d03d6d7faf30e962028eff4c1
StefaniakChristopher <stefaniakchristopher@gmail.com>
2024-10-22 20:38:53 -0400

    ctrl z

--- dashboard_app/app.py
+++ dashboard_app/app.py
@@ -1,55 +1,44 @@
-from flask import Flask, render_template, request, jsonify, g, session
+from flask import Flask, rendertemplate, request, jsonify, g
+app = Flask(name)
 import base64
 import asyncio
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Needed for session encryption
-
-def get_default_base64_image():
+def getdefaultbase64image():
     with open("uploads/parking-lot-facebook.jpg", "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
 
-@app.before_request
-def load_defaults():
-    # Set defaults in the session if they are not already there
-    if 'base64_image' not in session:
-        session['base64_image'] = get_default_base64_image()
-    if 'description' not in session:
-        session['description'] = "Fake parking lot"
+base64_image = get_default_base64_image()
+description = "Fake parking lot"
 
 @app.route('/')
 def home():
-    base64_image = session.get('base64_image')
-    description = session.get('description')
+    global base64_image 
+    global description 
     print("home endpoint: " + base64_image[:10])
+
     return render_template('index.html', base64_image=base64_image, description=description)
 
 @app.route('/submit', methods=['POST'])
 async def submit():
-    # Ensure the request contains JSON
+    global base64_image
+    global description
+
     if not request.is_json:
         return jsonify({'status': 'error', 'message': 'Request must be JSON'}), 400
-    
-    # Get the data from the request
+
     data = request.get_json()
-    description = data.get('description')
-    base64_image = data.get('image')
+    description = data.get('description', None)
+    base64_image = data.get('image', None)
 
     if not description or not base64_image:
         return jsonify({'status': 'error', 'message': 'Missing description or image data'}), 400
-    
-    # Update the session data
-    session['description'] = description
-    session['base64_image'] = base64_image
-    
-    print("submit endpoint: " + base64_image[:10])
-    
-    # Send a response indicating success
+
     response = {
         'status': 'success',
         'description': description,
+        'image': base64_image
     }
     return jsonify(response)
 
-if __name__ == '__main__':
+if __name == '__main':
     app.run(debug=True, host='0.0.0.0')
