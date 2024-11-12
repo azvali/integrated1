32233bd059419ad045f5be8837d13a6b39b02612
Chris <stefaniakchristopher@gmail.com>
2024-11-11 21:36:36 -0500

    k8s implementation

--- dockerpush.bat
+++ dockerpush.bat
@@ -2,8 +2,8 @@ cd ./dashboard_app
 
 docker build -t flask-app .
 
-docker tag flask-app:latest
+docker tag flask-app:latest chrisstefaniak/pkinglot-analyzer:latest
 
-docker push flask-app:latest
+docker push chrisstefaniak/pkinglot-analyzer:latest
 
 pause
