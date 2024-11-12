2671df4a9e228e62c5f68f6027f22abe985e835f
Chris <stefaniakchristopher@gmail.com>
2024-11-11 22:24:15 -0500

    updated monitoring

--- http_logs.sh
+++ http_logs.sh
@@ -9,15 +9,16 @@ log_resource_usage() {
 
     echo "NGINX Metrics:" >> $LOG_FILE
 
-    # Fetch logs from the container
-    access_log=$(kubectl logs <POD_NAME> -c <CONTAINER_NAME> --tail=1000)
+    # If pod is updated, may need to update the pod name
+    access_log=$(kubectl logs my-pod-name -c my-container-name --tail=1000)
 
     if [ -n "$access_log" ]; then
-        # Count total requests
         total_requests=$(echo "$access_log" | grep -c "^")
         echo "Total HTTP requests: $total_requests" >> $LOG_FILE
 
-        # Count response status codes
+        submit_requests=$(echo "$access_log" | grep "/submit" | wc -l)
+        echo "Total /submit requests: $submit_requests" >> $LOG_FILE
+
         status_200=$(echo "$access_log" | grep " 200 " | wc -l)
         status_400=$(echo "$access_log" | grep " 400 " | wc -l)
         status_404=$(echo "$access_log" | grep " 404 " | wc -l)
