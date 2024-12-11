40192330370bd652a77f769d3e20122a03be026a
Chris <stefaniakchristopher@gmail.com>
2024-12-11 15:59:37 -0500

    j ignore it

--- dockerpush.bat
+++ dockerpush.bat
@@ -2,8 +2,8 @@ cd ./dashboard_app
 
 docker build --no-cache -t flask-app .
 
-docker tag flask-app:latest chrisstefaniak/pkinglot-analyzer2:1.0.12
+docker tag flask-app:latest chrisstefaniak/pkinglot-analyzer2:1.0.13
 
-docker push chrisstefaniak/pkinglot-analyzer2:1.0.12
+docker push chrisstefaniak/pkinglot-analyzer2:1.0.13
 
 pause
